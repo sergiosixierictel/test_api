c117414f4eba66ad76266f1af1bb06ca4db1fc62
Sergio Sixi <sergio.sixi@erictel.com>
2018-03-15 12:54:43 -0500

    port changed

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const port = process.env.PORT || 9000;
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const cors = require('cors');
+const axios = require('axios');
 
 let app = express();
 
@@ -10,8 +11,6 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(methodOverride())
-app.set('port',(process.env.PORT || 9000));
-
 
 let api = express.Router();
 
@@ -20,13 +19,29 @@ api.route('/').get((req,res)=>{
 });
 
 api.route('/form').post((req,res)=>{
-    console.log('Cuerpo de la respuesta')
     console.log(req.body)
     res.status(200).send('Ok');
 });
+
+api.route('/data').post((req,res)=>{
+    let user = req.body.user;
+    let pass = req.body.pass;
+    let formId = req.body.formId;
+    let params = {
+        user:user,
+        pass:pass,
+        formId:formId
+    }
+    axios.post('https://apigeogestion.erictelm2m.com:7443/getForm',params)
+    .then(data=>{
+        res.status(200).send(data.data);
+    })
+    .catch(err=>{
+        res.status(500).send(err);
+    })
+})
 app.use('/api',api);
 
-
-app.listen(app.get('port'), ()=>{
+app.listen(port, ()=>{
     console.log(`Express Server Listening in ${port}`)
 })
